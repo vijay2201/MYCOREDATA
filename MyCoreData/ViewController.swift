6c1db020f5f5be8cd8c313dd5409fd968edaf496
COE-008 <coe-008@coe-008s-imac.local>
2020-01-01 14:53:37 +0530

    Initial Commit

---
+++ MyCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCoreData
+//
+//  Created by COE-008 on 01/01/20.
+//  Copyright © 2020 COE-008. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
